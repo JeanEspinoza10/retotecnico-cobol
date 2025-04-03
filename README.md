2e704992c65ea816a05fb46c9427f97429df5185
jespinoza <jeanria10@gmail.com>
2025-04-03 09:18:46 -0500

    add resolution for test techinal

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# retotecnico-cobol
-Reto técnico Cobol - Codeable
+# Introducción
+Reto técnico que consiste en crear una aplicación que procese un archivo CSV y genere un reporte de transacciones, con el propósito de aplicar y demostrar los conocimientos en algoritmos, programación orientada a objetos (POO) y mejores prácticas de programación.
+
+# Instrucciones de Ejecución
+Antes de ejecutar el programa debemos asegurarnos que tengamos instalado Python en nuestro sistema. Si no lo tenemos instalado, puedes descargarlo desde la página oficial de Python: <https://www.python.org/downloads/>. 
+
+Para ejecutar el programa, sigue los siguientes pasos:
+1. Abra una terminal o línea de comandos en tu sistema operativo.
+2. Crear un entorno de python virtual con el siguiente comando:
+```
+python -m venv venv
+```
+3. Activar el entorno de python virtual con el siguiente comando:
+```
+.\venv\Scripts\activate
+```
+4. Instalar las dependencias necesarias con el siguiente comando:
+```
+pip install -r requirements.txt
+```
+5. Ejecutar el programa con el siguiente comando:
+```
+python main.py --route ./data/example.csv
+```
+
+# Enfoque y Solución
+Durante el análisis de este reto técnico se identificó que uno de los aspectos más importante es como trataremos el archivo CSV. Para ello, se utilizó la librería Pandas, que es una librería de Python muy popular para manipular y analizar datos en formato CSV.
+
+
+### Algoritmo
+![Algoritmo](src/img/algoritmo.png)
+
+# Estructura del Proyecto
+
+### Archivo main.py
+Este archivo contiene la lógica principal del programa, incluyendo la creación de la clase Reporte y la ejecución del programa.
+
+### Carpeta data
+Contiene un archivo CSV de ejemplo que se utilizará para el análisis del reto técnico.
+
+### Archivo requirements.txt
+Contiene las dependencias necesarias para ejecutar el programa.
---
+++ main.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import argparse
+import os
+
+class Reporte:
+    def __init__(self, path_to_file):
+        self.df = self.load_data(path_to_file)
+        self.print_init()
+    
+    def print_init(self):
+        print(f"\nReporte de Transacciones\n{'-' * 45}\n")
+        
+    def load_data(self, path_to_file):
+        try:
+            df = pd.read_csv(path_to_file)
+            return df
+        except Exception as e:
+            raise Exception(f"Error in load_data: {e}")
+            
+    def get_final_balance(self):
+        try:
+            total_credit = 0
+            total_debit = 0
+            for index, row in self.df.iterrows():
+                if row["tipo"] == "Crédito":
+                    total_credit += row["monto"]
+                elif row["tipo"] == "Débito":
+                    total_debit += row["monto"]
+            balance_final = total_credit - total_debit
+            print("Balance Final:", balance_final, "\n")
+        except Exception as e:
+           print("Error in get_final_balance: ", e)
+            
+    def get_high_amount_transactions(self):
+        try:
+            value_high = 0
+            id_high = 0
+            for index, row in self.df.iterrows():
+                if row["monto"] > value_high:
+                    value_high = row["monto"]
+                    id_high = row["id"]
+            print(f"Transacción de Mayor Monto: ID {id_high} - {value_high}","\n")
+        except Exception as e:
+            print("Error in get_high_amount_transactions: ", e)
+    
+    def get_transaction_count(self):    
+        try:
+            unique_types = self.df["tipo"].unique()
+            count_type = {element: 0 for element in unique_types}
+            for index, row in self.df.iterrows():
+                if row["tipo"] in count_type:
+                    count_type[row["tipo"]] += 1
+            template = "Conteo de Transacciones:"
+            for key, value in count_type.items():
+                template += f" {key}: {value}"
+            print(template,"\n")
+        except Exception as e:
+            print("Error in get_transaction_count: ", e)
+            
+if __name__ == "__main__":
+    
+    try:
+        # Get the path to the file
+        parser = argparse.ArgumentParser(description="Report of Transactions")
+        parser.add_argument('--route', type=str, help="Route to the file")
+        args = parser.parse_args()
+        route_to_file = args.route
+        
+        # Generate the report
+        reporte = Reporte(route_to_file)
+        reporte.get_final_balance()
+        reporte.get_high_amount_transactions()
+        reporte.get_transaction_count()
+  
+    except Exception as e:
+        print("Error in main: ", e)
+  
